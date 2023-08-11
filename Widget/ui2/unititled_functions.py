a7f1de97214cd79d32c770305c5b3852b46a0bc2
zhang qian <128229843+sjeo@users.noreply.github.com>
2023-08-11 10:18:29 +0800

    Delete Widget directory

--- Widget/ui2/unititled_functions.py
+++
@@ -1,7 +0,0 @@
-class SOCKETFUNCTIONS:
-    def __init__(self, main_window):
-        self.main_window = main_window
-
-    def socket_basic(self):
-        self.main_window.ui.close.clicked.connect(self.main_window.close)
-
